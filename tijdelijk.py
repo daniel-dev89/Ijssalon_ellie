9b0a51289eb027f560a6d577e7e7c6081bc19f1b
Daniel van der Molen <daniellmetal89@gmail.com>
2025-11-15 10:07:36 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -10,4 +10,10 @@ reclame_tekst2 = reclame_tekst[:61]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING:", "VANILLE-IJS,", "1", "LITER-SLECHTS", "€", "2.4"]
 
-print(reclame_tekst4)
+
+for el in reclame_tekst4: 
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el)
+
