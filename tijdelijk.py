62e6e8eba7322a5ccbd811474e4956aa2f2c714e
Daniel van der Molen <daniellmetal89@gmail.com>
2025-11-17 21:47:03 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,25 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-aanbieding = prijzen["aardbei"] * 0.8
-
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter-slechts € {aanbieding}"
-reclame_tekst2 = reclame_tekst[:61]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING:", "VANILLE-IJS,", "1", "LITER-SLECHTS", "€", "2.4"]
+from helper import decoreer
 
 
-for el in reclame_tekst4: 
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el)
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    aanbieding = prijzen["aardbei"] * 0.8
+
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter, - ,slechts € {aanbieding}"
+    reclame_tekst2 = reclame_tekst[:61]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING:", "VANILLE-IJS,", "1", "LITER", "-", "SLECHTS", "€", "2.40"]
+
+    for el in reclame_tekst4: 
+        if len(el) >= 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
 
